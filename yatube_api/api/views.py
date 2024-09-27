bb953191679f1594f03307208c875ce4e1176914
Nastya Makarova <nastasia.konovalova@yandex.ru>
2024-09-27 23:33:18 +0400

    Added pagination for posts.

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework.pagination import LimitOffsetPagination
 
 from .permissions import OwnerOrReadOnly, ReadOnly
 from .serializers import PostSerializer
@@ -9,6 +10,7 @@ class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
     permission_classes = (OwnerOrReadOnly,)
+    pagination_class = LimitOffsetPagination
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
